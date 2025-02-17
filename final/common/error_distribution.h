adb98e476af9f6c0951a6ceab118afc91be2d1d4
史靖玮 Jingwei Shi <50292300+shi0712@users.noreply.github.com>
2025-02-17 15:48:56 +0800

    Update error_distribution.h

--- final/common/error_distribution.h
+++ final/common/error_distribution.h
@@ -23,6 +23,7 @@ using TheoreticalProbabilityFunc = function<double(double)>;
 // }
 
 double theoretical_probability_ours_tower(double epsilon, double phi_P_miss, double l) {
+    cout << phi_P_miss << " " << epsilon << " " << l << endl;
     return 1 - pow((phi_P_miss / epsilon / l), 3) / 8;
 }
 
@@ -31,11 +32,11 @@ double theoretical_probability_ours_cm(double epsilon, double phi_P_miss, double
 }
 
 double theoretical_probability_elastic_cm(double epsilon, double eta, double l) {
-    return 1 - pow((eta / epsilon / l), 3) / 8;
+    return 1 - pow((eta / epsilon / l), 3);
 }
 
 double theoretical_probability_elastic_tower(double epsilon, double eta, double l) {
-    return 1 - pow((eta / epsilon / l), 3);
+    return 1 - pow((eta / epsilon / l), 3) / 8;
 }
 
 double theoretical_probability_ladderfilter_cu(double epsilon, double n, double w_prime) {
@@ -55,10 +56,9 @@ public:
     error_distribution_record() {
         total_stream_size = 0;
         error_ratios.clear();
-        for (int i = 6; i > 0; i--) {
-            // double ratio = 1e-i;
-            double ratio = pow(10, -i);
-            error_ratios.push_back(ratio);
+        for (int i = 6; i <= 20; i+=1) {
+            double ratio = pow(10, -6);
+            error_ratios.push_back(ratio * i);
         }
         error_freqs.clear();
         save_path = "./error_distribution.csv";
@@ -79,13 +79,11 @@ public:
     }
 
     void insert_error(int error) {
-        // stream frequency estimation error must be non-negative integer
-        // assert(error >= 0);
         error_freqs[error]++;
         total_stream_size++;
     }
 
-    void dump_error_distribution() {
+    void dump_error_distribution(int packet_num) {
         FILE *pf = fopen(save_path.c_str(), "w");
         // column: different error ratio
         // row 1: estimated probability
@@ -103,7 +101,7 @@ public:
                 break;
             }
             // printf("current itr error_freq: %d %d\n", itr.first, itr.second);
-            int error_ratio_to_freq = total_stream_size * error_ratios[current_error_ratio_index];
+            int error_ratio_to_freq = packet_num * error_ratios[current_error_ratio_index];
             sum += itr.second;
             while (itr.first >= error_ratio_to_freq) {
                 fprintf(pf, "%.7lf,", (double)sum / total_stream_size);
@@ -111,7 +109,8 @@ public:
                 if (current_error_ratio_index >= error_ratios.size()) {
                     break;
                 }
-                error_ratio_to_freq = total_stream_size * error_ratios[current_error_ratio_index];
+                error_ratio_to_freq = packet_num * error_ratios[current_error_ratio_index];
+                // cout << "freq" << " " << error_ratio_to_freq << endl;
             }
         }
         while (current_error_ratio_index < error_ratios.size()) {
